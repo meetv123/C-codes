d563c771a1543d5ee6aed9edb692a60a60224e60
Meet Vanani <50437833+meetv123@users.noreply.github.com>
2021-05-07 13:11:37 +0530

    Add files via upload

---
+++ basic c++ practice codes/Call by pointer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+	int swapPointer(int* a, int* b){
+		int temp= *a;
+		*a= *b;
+		*b= temp;
+	}
+int main(){
+	int x=4, y=5;
+	cout<<"The value of a: "<<x<<" The value of b: "<<y<<endl;
+	swapPointer(&x,&y);
+	cout<<"The value of a: "<<x<<" The value of b: "<<y<<endl;
+	
+	return 0;
+}	
+
